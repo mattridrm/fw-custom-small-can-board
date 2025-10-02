cb64a6d0f1d60286cacf926fa424217421ad16b1
GitHub git update Action <action@github.com>
2025-10-02 01:32:43 +0000

    Custom Auto-generated Configs
    [no ci]

--- generated/controllers/generated/rusefi_generated_small-can-board.h
+++ generated/controllers/generated/rusefi_generated_small-can-board.h
@@ -2075,7 +2075,7 @@
 #define ts_show_wastegate_sensor true
 #define ts_show_wbo_canbus_index true
 #define ts_show_wbo_canbus_set_index true
-#define TS_SIGNATURE "rusEFI main.2025.10.01.small-can-board.1424965998"
+#define TS_SIGNATURE "rusEFI main.2025.10.02.small-can-board.1424965998"
 #define TS_SIMULATE_CAN '>'
 #define TS_SIMULATE_CAN_char >
 #define TS_TEST_COMMAND 't'
