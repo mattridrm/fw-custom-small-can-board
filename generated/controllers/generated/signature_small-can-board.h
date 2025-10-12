57e18d246c91c9ec2af8de1498b75b8d7349d26c
GitHub git update Action <action@github.com>
2025-10-12 01:45:40 +0000

    Custom Auto-generated Configs
    [no ci]

--- generated/controllers/generated/signature_small-can-board.h
+++ generated/controllers/generated/signature_small-can-board.h
@@ -3,4 +3,4 @@
 //
 
 #define SIGNATURE_HASH 1424965998
-#define TS_SIGNATURE "rusEFI main.2025.10.11.small-can-board.1424965998"
+#define TS_SIGNATURE "rusEFI main.2025.10.12.small-can-board.1424965998"
